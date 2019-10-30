76c145dd298e0bd70c33d093f1e031a7e81e2bd4
hilal567 <33893219+hilal567@users.noreply.github.com>
2019-10-30 20:19:02 +0300

    this upto where sianwa reached in the login and registration.

---
+++ app/src/main/java/com/example/sote/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.example.sote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * Logs in the user into their account
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/example/sote/SetupActivity.java
@@ -0,0 +1,17 @@
+package com.example.sote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * A class that allows user to set up their profile details
+ */
+public class SetupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup);
+    }
+}
