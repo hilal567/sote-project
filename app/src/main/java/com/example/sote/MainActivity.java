f32350b13ac85f352b583ad378bf0305d774eff3
hilal567 <33893219+hilal567@users.noreply.github.com>
2019-11-07 14:06:28 +0300

    UI

--- app/src/main/java/com/example/sote/MainActivity.java
+++ app/src/main/java/com/example/sote/MainActivity.java
@@ -16,6 +16,7 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
